254cf28ffbc962ffa87b57e7876d7dfacacfdc4c
amalej <alejandroalejandre15@gmail.com>
2023-10-09 05:23:04 +0800

    Fix tests

--- tests/jsonFileToEnvFile.test.ts
+++ tests/jsonFileToEnvFile.test.ts
@@ -1,120 +1,134 @@
 import path from "path";
 import { jsonFileToEnvFile } from "../src";
 import { config } from "dotenv";
-import { readdir, unlink } from "fs/promises";
+import { mkdir, readdir, rmdir, stat, unlink } from "fs/promises";
 
 describe("Test reading JSON files", () => {
+  beforeAll(async () => {
+    const outDir = path.join(__dirname, "jsonFileToEnvFiles");
+    try {
+      await stat(outDir);
+    } catch (err) {
+      if (err.code === "ENOENT") {
+        await mkdir(outDir);
+      } else {
+        throw new Error(`Unknown error occured: ${err.message}`);
+      }
+    }
+  });
+
   it("Shoud parse the JSON file and output to env ENV file", async () => {
-    const outDir = path.join(__dirname, "jsonFileToEnvFiles", ".env.0");
+    const outFilePath = path.join(__dirname, "jsonFileToEnvFiles", ".env.0");
     await jsonFileToEnvFile(
       path.join(__dirname, "jsonFiles", "0.json"),
-      outDir
+      outFilePath
     );
 
-    config({ path: outDir, override: true });
+    config({ path: outFilePath, override: true });
 
     expect(process.env.PROJECT_ID).toEqual("this-is-a-project-id");
   });
 
-  //   it("Shoud parse the JSON file and output to env ENV file", async () => {
-  //     const outDir = path.join(__dirname, "jsonFileToEnvFiles", ".env.1");
-  //     await jsonFileToEnvFile(
-  //       path.join(__dirname, "jsonFiles", "1.json"),
-  //       outDir
-  //     );
+  it("Shoud parse the JSON file and output to env ENV file", async () => {
+    const outFilePath = path.join(__dirname, "jsonFileToEnvFiles", ".env.1");
+    await jsonFileToEnvFile(
+      path.join(__dirname, "jsonFiles", "1.json"),
+      outFilePath
+    );
 
-  //     config({ path: outDir, override: true });
+    config({ path: outFilePath, override: true });
 
-  //     expect(process.env.PROJECT_ID).toEqual("this-is-a-project-id");
-  //     expect(process.env.API_KEY).toEqual("fake-api-key");
-  //     expect(process.env.APP_ID).toEqual("fake-app-id");
-  //   });
+    expect(process.env.PROJECT_ID).toEqual("this-is-a-project-id");
+    expect(process.env.API_KEY).toEqual("fake-api-key");
+    expect(process.env.APP_ID).toEqual("fake-app-id");
+  });
 
-  //   it("Shoud parse the JSON file and output to env ENV file", async () => {
-  //     const outDir = path.join(__dirname, "jsonFileToEnvFiles", ".env.2");
-  //     await jsonFileToEnvFile(
-  //       path.join(__dirname, "jsonFiles", "2.json"),
-  //       outDir
-  //     );
+  it("Shoud parse the JSON file and output to env ENV file", async () => {
+    const outFilePath = path.join(__dirname, "jsonFileToEnvFiles", ".env.2");
+    await jsonFileToEnvFile(
+      path.join(__dirname, "jsonFiles", "2.json"),
+      outFilePath
+    );
 
-  //     config({ path: outDir, override: true });
+    config({ path: outFilePath, override: true });
 
-  //     expect(process.env.PROJECT_ID).toEqual("this-is-a-project-id");
-  //     expect(process.env.ITEM_0).toEqual("item 0");
-  //     expect(process.env.ITEM_1).toEqual("item 1");
-  //     expect(process.env.ITEM_2).toEqual("item 2");
-  //   });
+    expect(process.env.PROJECT_ID).toEqual("this-is-a-project-id");
+    expect(process.env.ITEM_0).toEqual("item 0");
+    expect(process.env.ITEM_1).toEqual("item 1");
+    expect(process.env.ITEM_2).toEqual("item 2");
+  });
 
-  //   it("Shoud parse the JSON file and output to env ENV file", async () => {
-  //     const outDir = path.join(__dirname, "jsonFileToEnvFiles", ".env.3");
-  //     await jsonFileToEnvFile(
-  //       path.join(__dirname, "jsonFiles", "3.json"),
-  //       outDir
-  //     );
+  it("Shoud parse the JSON file and output to env ENV file", async () => {
+    const outFilePath = path.join(__dirname, "jsonFileToEnvFiles", ".env.3");
+    await jsonFileToEnvFile(
+      path.join(__dirname, "jsonFiles", "3.json"),
+      outFilePath
+    );
 
-  //     config({ path: outDir, override: true });
+    config({ path: outFilePath, override: true });
 
-  //     expect(process.env.PROJECT_ID).toEqual("this-is-a-project-id");
-  //     expect(process.env.ITEM_0).toEqual("item 0");
-  //     expect(process.env.ITEM_1).toEqual("item 1");
-  //     expect(process.env.ITEM_2).toEqual("item 2");
-  //     expect(process.env.APP_ID).toEqual("fake-app-id");
-  //   });
+    expect(process.env.PROJECT_ID).toEqual("this-is-a-project-id");
+    expect(process.env.ITEM_0).toEqual("item 0");
+    expect(process.env.ITEM_1).toEqual("item 1");
+    expect(process.env.ITEM_2).toEqual("item 2");
+    expect(process.env.APP_ID).toEqual("fake-app-id");
+  });
 
-  //   it("Shoud parse the JSON file and output to env ENV file", async () => {
-  //     const outDir = path.join(__dirname, "jsonFileToEnvFiles", ".env.4");
-  //     await jsonFileToEnvFile(
-  //       path.join(__dirname, "jsonFiles", "4.json"),
-  //       outDir
-  //     );
+  it("Shoud parse the JSON file and output to env ENV file", async () => {
+    const outFilePath = path.join(__dirname, "jsonFileToEnvFiles", ".env.4");
+    await jsonFileToEnvFile(
+      path.join(__dirname, "jsonFiles", "4.json"),
+      outFilePath
+    );
 
-  //     config({ path: outDir, override: true });
+    config({ path: outFilePath, override: true });
 
-  //     expect(process.env.PROJECT_ID).toEqual("this-is-a-project-id");
-  //     expect(process.env.ITEM_0).toEqual("item 0");
-  //     expect(process.env.ITEM_1).toEqual("item 1");
-  //     expect(process.env.ITEM_2).toEqual("item 2");
-  //     expect(process.env.APP_ID).toEqual("fake-app-id");
-  //     expect(process.env.TOKENS_0).toEqual("abc-token");
-  //     expect(process.env.TOKENS_1).toEqual("def-token");
-  //     expect(process.env.TOKENS_2).toEqual("ghi-token");
-  //   });
+    expect(process.env.PROJECT_ID).toEqual("this-is-a-project-id");
+    expect(process.env.ITEM_0).toEqual("item 0");
+    expect(process.env.ITEM_1).toEqual("item 1");
+    expect(process.env.ITEM_2).toEqual("item 2");
+    expect(process.env.APP_ID).toEqual("fake-app-id");
+    expect(process.env.TOKENS_0).toEqual("abc-token");
+    expect(process.env.TOKENS_1).toEqual("def-token");
+    expect(process.env.TOKENS_2).toEqual("ghi-token");
+  });
 
-  //   it("Shoud parse the JSON file and output to env ENV file", async () => {
-  //     const outDir = path.join(__dirname, "jsonFileToEnvFiles", ".env.5");
-  //     await jsonFileToEnvFile(
-  //       path.join(__dirname, "jsonFiles", "5.json"),
-  //       outDir
-  //     );
+  it("Shoud parse the JSON file and output to env ENV file", async () => {
+    const outFilePath = path.join(__dirname, "jsonFileToEnvFiles", ".env.5");
+    await jsonFileToEnvFile(
+      path.join(__dirname, "jsonFiles", "5.json"),
+      outFilePath
+    );
 
-  //     config({ path: outDir, override: true });
+    config({ path: outFilePath, override: true });
 
-  //     expect(process.env.PROJECT_ID).toEqual("this-is-a-project-id");
-  //     expect(process.env.ITEM_0).toEqual("item 0");
-  //     expect(process.env.ITEM_1).toEqual("item 1");
-  //     expect(process.env.ITEM_2).toEqual("item 2");
-  //     expect(process.env.APP_ID).toEqual("fake-app-id");
-  //     expect(process.env.TOKENS_0).toEqual("abc-token");
-  //     expect(process.env.TOKENS_1).toEqual("def-token");
-  //     expect(process.env.TOKENS_2).toEqual("ghi-token");
-  //     expect(process.env.USERS_0_NAME).toEqual("Uno");
-  //     expect(process.env.USERS_0_EMAIL).toEqual("uno@gmail.com");
-  //     expect(process.env.USERS_1_NAME).toEqual("Dos");
-  //     expect(process.env.USERS_1_EMAIL).toEqual("dos@gmail.com");
-  //   });
+    expect(process.env.PROJECT_ID).toEqual("this-is-a-project-id");
+    expect(process.env.ITEM_0).toEqual("item 0");
+    expect(process.env.ITEM_1).toEqual("item 1");
+    expect(process.env.ITEM_2).toEqual("item 2");
+    expect(process.env.APP_ID).toEqual("fake-app-id");
+    expect(process.env.TOKENS_0).toEqual("abc-token");
+    expect(process.env.TOKENS_1).toEqual("def-token");
+    expect(process.env.TOKENS_2).toEqual("ghi-token");
+    expect(process.env.USERS_0_NAME).toEqual("Uno");
+    expect(process.env.USERS_0_EMAIL).toEqual("uno@gmail.com");
+    expect(process.env.USERS_1_NAME).toEqual("Dos");
+    expect(process.env.USERS_1_EMAIL).toEqual("dos@gmail.com");
+  });
 
-  //   afterAll(async () => {
-  //     const dirPath = path.join(__dirname, "jsonFileToEnvFiles");
-  //     try {
-  //       const files = await readdir(dirPath);
+  afterAll(async () => {
+    const dirPath = path.join(__dirname, "jsonFileToEnvFiles");
+    try {
+      const files = await readdir(dirPath);
 
-  //       const deleteFilePromises = files.map((file) =>
-  //         unlink(path.join(dirPath, file))
-  //       );
+      const deleteFilePromises = files.map((file) =>
+        unlink(path.join(dirPath, file))
+      );
 
-  //       await Promise.all(deleteFilePromises);
-  //     } catch (err) {
-  //       console.log(err);
-  //     }
-  //   });
+      await Promise.all(deleteFilePromises);
+      await rmdir(dirPath);
+    } catch (err) {
+      console.log(err);
+    }
+  });
 });
