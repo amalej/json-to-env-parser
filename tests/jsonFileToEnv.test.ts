424eccf2c22eeb63b412e52cf9e21e8abd0ef310
amalej <alejandroalejandre15@gmail.com>
2023-10-09 04:25:53 +0800

    Added test script

---
+++ tests/jsonFileToEnv.test.ts
@@ -0,0 +1,65 @@
+import path from "path";
+import { jsonFileToEnv } from "../src";
+
+describe("Test reading JSON files", () => {
+  it("Shoud parse the JSON file and output it in an env format", async () => {
+    const env = await jsonFileToEnv(
+      path.join(__dirname, "jsonFiles", "0.json")
+    );
+
+    expect(env).toEqual("PROJECT_ID=this-is-a-project-id\n");
+  });
+
+  it("Shoud parse the JSON file and output it in an env format", async () => {
+    const env = await jsonFileToEnv(
+      path.join(__dirname, "jsonFiles", "1.json")
+    );
+
+    expect(env).toEqual(
+      "PROJECT_ID=this-is-a-project-id\nAPI_KEY=fake-api-key\nAPP_ID=fake-app-id\n"
+    );
+  });
+
+  it("Shoud parse the JSON file and output it in an env format", async () => {
+    const env = await jsonFileToEnv(
+      path.join(__dirname, "jsonFiles", "2.json")
+    );
+
+    expect(env).toEqual(
+      "PROJECT_ID=this-is-a-project-id\nITEM_0=item 0\nITEM_1=item 1\nITEM_2=item 2\n"
+    );
+  });
+
+  it("Shoud parse the JSON file and output it in an env format", async () => {
+    const env = await jsonFileToEnv(
+      path.join(__dirname, "jsonFiles", "3.json")
+    );
+
+    expect(env).toEqual(
+      "PROJECT_ID=this-is-a-project-id\nITEM_0=item 0\nITEM_1=item 1\nITEM_2=item 2\nAPP_ID=fake-app-id\n"
+    );
+  });
+
+  it("Shoud parse the JSON file and output it in an env format", async () => {
+    const env = await jsonFileToEnv(
+      path.join(__dirname, "jsonFiles", "4.json")
+    );
+
+    expect(env).toEqual(
+      `PROJECT_ID=this-is-a-project-id\nITEM_0=item 0\nITEM_1=item 1\nITEM_2=item 2\nAPP_ID=fake-app-id
+TOKENS_0=abc-token\nTOKENS_1=def-token\nTOKENS_2=ghi-token\n`
+    );
+  });
+
+  it("Shoud parse the JSON file and output it in an env format", async () => {
+    const env = await jsonFileToEnv(
+      path.join(__dirname, "jsonFiles", "5.json")
+    );
+
+    expect(env).toEqual(
+      `PROJECT_ID=this-is-a-project-id\nITEM_0=item 0\nITEM_1=item 1\nITEM_2=item 2\nAPP_ID=fake-app-id
+TOKENS_0=abc-token\nTOKENS_1=def-token\nTOKENS_2=ghi-token
+USERS_0_NAME=Uno\nUSERS_0_EMAIL=uno@gmail.com\nUSERS_1_NAME=Dos\nUSERS_1_EMAIL=dos@gmail.com\n`
+    );
+  });
+});
