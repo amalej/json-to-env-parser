d3e2bb2b14f005f284053f4b2fe011e582153c45
amalej <alejandroalejandre15@gmail.com>
2023-10-09 01:35:12 +0800

    Added tests

--- tests/jsonToEnv.test.ts
+++ tests/jsonToEnv.test.ts
@@ -29,7 +29,7 @@ const simpleJsonEnvs = [
   },
 ];
 
-const normalJsonWithArrEnvs = [
+const jsonWithArrEnvs = [
   {
     json: {
       project_id: "this-is-a-project-id",
@@ -57,31 +57,27 @@ TOKENS_0=abc-token\nTOKENS_1=def-token\nTOKENS_2=ghi-token\n`,
   },
 ];
 
-const normalJsonWithNestedValEnvs = [
-  {
-    json: {
-      project_id: "this-is-a-project-id",
-      item: ["item 0", "item 1", "item 2"],
-    },
-    env: `PROJECT_ID=this-is-a-project-id\nITEM_0=item 0\nITEM_1=item 1\nITEM_2=item 2\n`,
-  },
-  {
-    json: {
-      project_id: "this-is-a-project-id",
-      item: ["item 0", "item 1", "item 2"],
-      app_id: "fake-app-id",
-    },
-    env: `PROJECT_ID=this-is-a-project-id\nITEM_0=item 0\nITEM_1=item 1\nITEM_2=item 2\nAPP_ID=fake-app-id\n`,
-  },
+const jsonWithNestedValEnvs = [
   {
     json: {
       project_id: "this-is-a-project-id",
       item: ["item 0", "item 1", "item 2"],
       app_id: "fake-app-id",
       tokens: ["abc-token", "def-token", "ghi-token"],
+      users: [
+        {
+          name: "Uno",
+          email: "uno@gmail.com",
+        },
+        {
+          name: "Dos",
+          email: "dos@gmail.com",
+        },
+      ],
     },
     env: `PROJECT_ID=this-is-a-project-id\nITEM_0=item 0\nITEM_1=item 1\nITEM_2=item 2\nAPP_ID=fake-app-id
-TOKENS_0=abc-token\nTOKENS_1=def-token\nTOKENS_2=ghi-token\n`,
+TOKENS_0=abc-token\nTOKENS_1=def-token\nTOKENS_2=ghi-token
+USERS_0_NAME=Uno\nUSERS_0_EMAIL=uno@gmail.com\nUSERS_1_NAME=Dos\nUSERS_1_EMAIL=dos@gmail.com\n`,
   },
 ];
 
@@ -94,8 +90,8 @@ describe("Convert simple JSON to ENV format", () => {
   }
 });
 
-describe("Convert normal JSON with arrays to ENV format", () => {
-  for (let jsonEnv of normalJsonWithArrEnvs) {
+describe("Convert JSON with arrays to ENV format", () => {
+  for (let jsonEnv of jsonWithArrEnvs) {
     it("Should correctly convert normal json with arrays", () => {
       const env = jsonToEnv(jsonEnv.json);
       expect(env).toEqual(jsonEnv.env);
@@ -103,8 +99,8 @@ describe("Convert normal JSON with arrays to ENV format", () => {
   }
 });
 
-describe("Convert normal JSON with nested values to ENV format", () => {
-  for (let jsonEnv of normalJsonWithNestedValEnvs) {
+describe("Convert JSON with nested values to ENV format", () => {
+  for (let jsonEnv of jsonWithNestedValEnvs) {
     it("Should correctly convert normal json with nested values", () => {
       const env = jsonToEnv(jsonEnv.json);
       expect(env).toEqual(jsonEnv.env);
